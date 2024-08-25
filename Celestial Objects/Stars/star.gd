036b3010f3563461008d0a5ffeb7e0c7a2e61501
Pekersen <pekersen8@gmail.com>
2024-08-25 16:44:55 -0500

    Added color for stars based on star type
    Every star type now has a range of values for RGB  from which the generation randomly picks from. Additionally, the light color uses a dampened version of these values.

--- Celestial Objects/Stars/star.gd
+++ Celestial Objects/Stars/star.gd
@@ -7,11 +7,16 @@ extends Star
 
 func _on_system_star_gen(star_type):
 	print("Star Type: " + star_type)
+	
 	if star_type == "O":
 		radius_variance = [13.2,20]
 		mass_variance = [16,20]
 		luminosity_variance = [50,70]
 		temperature_variance = [33000, 50000]
+		
+		color_variance_r = [0.60,0.685]
+		color_variance_g = [1,1]
+		color_variance_b = [1,1]
 		
 	elif star_type == "B":
 		radius_variance = [3.6,13.2]
@@ -19,11 +24,19 @@ func _on_system_star_gen(star_type):
 		luminosity_variance = [40,50]
 		temperature_variance = [10000, 33000]
 		
+		color_variance_r = [0.685,0.875]
+		color_variance_g = [1,1]
+		color_variance_b = [1,1]
+		
 	elif star_type == "A":
 		radius_variance = [2.8,3.6]
 		mass_variance = [1.4,2.1]
 		luminosity_variance = [20,30]
 		temperature_variance = [7300, 10000]
+		
+		color_variance_r = [0.875,1]
+		color_variance_g = [1,1]
+		color_variance_b = [1,1]
 		
 	elif star_type == "F":
 		radius_variance = [2.2,2.8]
@@ -31,11 +44,19 @@ func _on_system_star_gen(star_type):
 		luminosity_variance = [10,20]
 		temperature_variance = [6000, 7300]
 		
+		color_variance_r = [1,1]
+		color_variance_g = [1,1]
+		color_variance_b = [0.8,1]
+		
 	elif star_type == "G":
 		radius_variance = [1.8,2.2]
 		mass_variance = [0.8,1.04]
 		luminosity_variance = [5,10]
 		temperature_variance = [5300, 6000]
+		
+		color_variance_r = [1,1]
+		color_variance_g = [1,1]
+		color_variance_b = [0.6,0.8]
 		
 	elif star_type == "K":
 		radius_variance = [1.4,1.8]
@@ -43,26 +64,34 @@ func _on_system_star_gen(star_type):
 		luminosity_variance = [1,5]
 		temperature_variance = [3900, 5300]
 		
+		color_variance_r = [1,1]
+		color_variance_g = [0.7,0.8]
+		color_variance_b = [0.4,0.6]
+		
 	elif star_type == "M":
 		radius_variance = [1,1.4]
 		mass_variance = [0.08,0.45]
 		luminosity_variance = [0.5,1]
 		temperature_variance = [2300, 3900]
 		
-	print(radius_variance)
-	print(mass_variance)
-	print(luminosity_variance)
-	print(temperature_variance)
+		color_variance_r = [1,1]
+		color_variance_g = [0.25,0.35]
+		color_variance_b = [0.2,0.3]
 		
 	radius = offsetValue(radius_variance)
 	mass = offsetValue(mass_variance)
 	luminosity = offsetValue(luminosity_variance)
 	temperature = offsetValue(temperature_variance)
 	
+	color_r = offsetValue(color_variance_r)
+	color_g = offsetValue(color_variance_g)
+	color_b = offsetValue(color_variance_b)
+	
 	print("Radius: " + str(radius))
 	print("Mass: " + str(mass))
 	print("Luminosity: " + str(luminosity))
 	print("Temperature: " + str(temperature))
+	print("Color: R-" + str(color_r) + ", G-" + str(color_g) + ", B-" + str(color_b) + ", ")
 	
 	starMesh.mesh.radius = radius
 	starMesh.mesh.height = radius * 2
@@ -71,6 +100,12 @@ func _on_system_star_gen(star_type):
 	
 	starLight.light_energy = luminosity
 	
+	var light_color_r = clamp(color_r + 0.3, 0.5, 1)
+	var light_color_g = clamp(color_g + 0.3, 0.5, 1)
+	var light_color_b = clamp(color_b + 0.3, 0.5, 1)
+	
+	starMesh.mesh.material.set_shader_parameter("Sun_Color", Color(color_r, color_g, color_b))
+	starLight.light_color = Color(light_color_r, light_color_g, light_color_b)
 	
 func _init():
 	pass
--- Celestial Objects/Stars/stars_script.gd
+++ Celestial Objects/Stars/stars_script.gd
@@ -1,7 +1,14 @@
 class_name Star extends CelestialObject
 
 var luminosity : float
-var num_planets : int
-var num_asteroids : int
+#var num_planets : int
+#var num_asteroids : int
+
+var color_r : float
+var color_g : float
+var color_b : float
 
 var luminosity_variance : Array[float]
+var color_variance_r : Array[float]
+var color_variance_g : Array[float]
+var color_variance_b : Array[float]
