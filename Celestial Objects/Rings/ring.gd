de0c9dc1bf3eb4d70b279fa9a4aadd96348e8418
Wqsk <weskg.weskg@gmail.com>
2024-11-16 11:24:36 -0600

    Revert "Attempt at Rings, unsuccessful so far"
    This reverts commit 39e164d2c3736f138c295761848fe7470e4fc92b.

--- Celestial Objects/Rings/ring.gd
+++ Celestial Objects/Rings/ring.gd
@@ -1,13 +1,12 @@
-extends Planet
+extends Node3D
 
-@onready var ring_mesh = $Torus
+@onready var ring_mesh := $Torus
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Ring Size 3: " + str(semi_major_axis))
-	ring_mesh.mesh.outer_radius = semi_major_axis + 0.0025
-	ring_mesh.mesh.inner_radius = semi_major_axis - 0.0025
-	
+	pass # Replace with function body.
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- Generation/generate_system.gd
+++ Generation/generate_system.gd
@@ -105,10 +105,9 @@ func initPlanetChildren():
 			var moonInstance = initMoon()
 			planetInstance.get_node("RotationPoint/Core").add_child(moonInstance)
 		
-		for k in range(num_rings):
+		for j in range(num_rings):
 			var ringInstance = initRing()
 			planetInstance.get_node("RotationPoint/Core").add_child(ringInstance)
-			print("Ring Size 2: " + str(ring_size))
 
 func initPlanetVars(planetInstance):
 	# TEMP VALUES
@@ -151,8 +150,7 @@ func initPlanetVars(planetInstance):
 	num_moons = offsetValue(num_moons_variance)
 	
 	num_rings = offsetValue(num_rings_variance)
-	ring_size_variance = [0.1, 0.3]
-	print("Number of rings: " + str(num_rings))
+	ring_size_variance = [radius + 0.1, radius + 0.4]
 	
 	print("Generate System script radius: " + str(planetInstance.radius))
 	print("Generate System script orbital_period: " + str(planetInstance.orbital_period))
@@ -184,5 +182,7 @@ func initRing():
 	
 func initRingVars(ringInstance):
 	ring_size = offsetValue(ring_size_variance)
-	ringInstance.semi_major_axis = ring_size + radius
-	print("Ring Size: " + str(ring_size))
+	ring.inner_radius = ring_size - 0.05
+	ring.outer_radius = ring_size + 0.05
+	
+	
