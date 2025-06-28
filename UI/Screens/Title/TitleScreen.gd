7e88d898345ea922ef227d85c3a9e45f8433a126
Wqsk <weskg.weskg@gmail.com>
2025-06-27 22:21:43 -0500

    Settings addon + fun surprise :D

--- UI/Screens/Title/TitleScreen.gd
+++ UI/Screens/Title/TitleScreen.gd
@@ -2,6 +2,7 @@ extends Control
 @onready var settings_screen := $SettingsScreen
 @onready var splash_screen := $SplashScreen
 @onready var bg = $Background
+@onready var title_music := $TitleMusic
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,15 +20,16 @@ func _ready():
 	else:
 		splash_screen.visible = false
 		bg.visible = true
-	
-	
-	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
+	random_float = rng.randf()
+	if random_float < 0.33:
+		title_music.stream = load("res://UI/Screens/Title/Assets/Interplanetary.mp3")
+	elif random_float < 0.66:
+		title_music.stream = load("res://UI/Screens/Title/Assets/Heartbeat of a Star.mp3")
+	else:
+		title_music.stream = load("res://UI/Screens/Title/Assets/Arriving Fleet.mp3")
+	title_music.play()	
+	var tween = get_tree().create_tween()
+	tween.tween_property(title_music, "volume_db", -20, 3)
 
 func _on_settings_pressed():
 	settings_screen.show()
